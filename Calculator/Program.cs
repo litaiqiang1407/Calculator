f77297a8f6d511589845edd97ab151aa69d57737
Ly Thai Cuong <ltc1407.5532@gmail.com>
2024-06-06 09:45:54 +0700

    Commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello Anh Em Toai la KunKun dei");
+Console.WriteLine("Hello Anh Em Toai la KunKun dei");
+
+Console.WriteLine("Hello Anh Em Toai la KunKun dei");
+Console.WriteLine("Hello Anh Em Toai la KunKun dei");
+
+
+
 
