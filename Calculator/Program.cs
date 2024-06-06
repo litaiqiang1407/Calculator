ed566eb0de0a8e2fda3d2e77d160745a0691c2ad
popivn <popixworkspace@gmail.com>
2024-06-06 09:08:01 +0700

    hehe
    hahaha

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose an operation (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = calculator.Add(num1, num2);
+                    break;
+                case "-":
+                    result = calculator.Subtract(num1, num2);
+                    break;
+                case "*":
+                    result = calculator.Multiply(num1, num2);
+                    break;
+                case "/":
+                    result = calculator.Divide(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    break;
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
