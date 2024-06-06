0c4f9674c271781f41827474a3af788a1776f3c8
truongvutrieu <146895259+truongvutrieu@users.noreply.github.com>
2024-06-06 10:15:50 +0700

    vcdgfd

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Truong Vu Trieu");
+            Console.WriteLine("ehhehehehehehehehhehehehgeheheheh");
+            Console.WriteLine("heheheheheheheheheheheheheheheheheheheheheheheheheheh");
+        }
+    }
+}
