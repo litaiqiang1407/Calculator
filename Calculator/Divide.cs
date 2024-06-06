251c22faa85a435bfe6ea2396250f1b7f835feb0
Ly Thai Cuong <ltc1407.5532@gmail.com>
2024-06-06 10:20:33 +0700

    Commit

---
+++ Calculator/Divide.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Divide
+    {
+        public static int DivideTwoNumbers(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+    }
+}
